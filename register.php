f51501744a284cc7eff61caea4fb0e29f5cfa7f8
RAM111111 <32421153+RAM111111@users.noreply.github.com>
2020-09-13 07:15:35 +0300

    Start coding

---
+++ register.php
@@ -0,0 +1,29 @@
+
+<?php include 'header.php'; ?>
+
+      <section id="register">
+        <div class="row m-0">
+          <div class="col-lg-4 offset-lg-2">
+            <div class="text-center pb-5">
+              <h1 class="login-title text-dark">Rigester</h1>
+              <p class="p-1 m-0 font-ubuntu text-black-50">Rigester and enjoy,,</p>
+              <span class="font-ubuntu text-black-50">I already have<a href="login.php">login</a></span>
+            </div>
+            <div class="uploud-profile-img">
+              <div class="text-center">
+                <div class="d-flex justify-content-center">
+                  <img class="camera-icon" src="assets/photo-camera.png" alt="img1">
+                </div>
+                <img class="img rounded-circle" src="assets/img-1.png" alt="img1" width="70%">
+                <small class="form-text text-black-50">Choose image</small>
+                <input type="file" name="profiluploud" class="form-control-file" id="uploud-profile">
+              </div>
+
+            </div>
+          </div>
+        </div>
+
+
+      </section>
+
+<?php include 'footer.php'; ?>
