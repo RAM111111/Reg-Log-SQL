0310d1199dac6c924c999626f93860651999e79b
RAM111111 <32421153+RAM111111@users.noreply.github.com>
2020-09-13 10:15:15 +0300

    add images

--- register.php
+++ register.php
@@ -1,7 +1,7 @@
 
 <?php include 'header.php'; ?>
 
-      <section id="register">
+      <section class="register">
         <div class="row m-0">
           <div class="col-lg-4 offset-lg-2">
             <div class="text-center pb-5">
@@ -9,12 +9,13 @@
               <p class="p-1 m-0 font-ubuntu text-black-50">Rigester and enjoy,,</p>
               <span class="font-ubuntu text-black-50">I already have<a href="login.php">login</a></span>
             </div>
-            <div class="uploud-profile-img">
+            <div class="uploud-profile-img d-flex justify-content-center pb-5">
               <div class="text-center">
                 <div class="d-flex justify-content-center">
-                  <img class="camera-icon" src="assets/photo-camera.png" alt="img1">
+                  <i class="fas fa-camera camera-icon"></i>
+
                 </div>
-                <img class="img rounded-circle" src="assets/img-1.png" alt="img1" width="70%">
+                <img class="img rounded-circle" src="assets/img1.svg" width="200px" height="200px" alt="img1">
                 <small class="form-text text-black-50">Choose image</small>
                 <input type="file" name="profiluploud" class="form-control-file" id="uploud-profile">
               </div>
